363051518bd9bfd8e35107620a77e38640034158
HanaPark1 <ydjxlst@gmail.com>
2025-03-31 18:31:17 +0900

    study: random.shuffle과 while문 학습

---
+++ Day4/py0331_06.py
@@ -0,0 +1,11 @@
+# X 좌표 : 1
+# Y 좌표 : 3
+# [1,3]
+
+num1 = int(input("X 좌표: "))
+num2 = int(input("Y 좌표: "))
+print(f"[X,Y 좌표: {num1} {num2}]")
+
+num = input("X,Y 좌표 (0,0) : ")
+n_list = num.split(",")
+print(f"[X,Y 좌표: {n_list}]")
