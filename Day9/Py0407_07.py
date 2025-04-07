2b7cdf1b6a6deeb7bdc2e86c3938d643ee362d44
HanaPark1 <ydjxlst@gmail.com>
2025-04-07 18:41:08 +0900

    study: class 상속

---
+++ Day9/Py0407_07.py
@@ -0,0 +1,15 @@
+class Car:
+    speed = 0
+    tire = 4
+    door = 5
+    def speedUp(self, s):
+        self.speed += s
+        print("현재 속도 ",self.speed)
+        
+class Sedan(Car):
+    color = "red"
+        
+c = Car()
+
+s = Sedan(Car)
+s.color = "red"
